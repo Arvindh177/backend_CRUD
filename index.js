0884f081a9d52d90e5a0307d89561dae1e53140a
Arvindh M <72850966+Arvindh177@users.noreply.github.com>
2023-12-17 10:05:44 +0530

    Update index.js
    Atlas changes

--- index.js
+++ index.js
@@ -8,44 +8,46 @@ require('dotenv').config();
 const mongo_URI = process.env.MONGO_URL;
 
 mongoose
-.connect(mongo_URI)
-.then((x)=>{
-    console.log(`Connected to Mongo Database name: "${x.connections[0].name}"`)
-})
-.catch((err)=> {
-    console.error('Error connect to mongo',err.reason)
-})
-//VdeFyjed6b5nl9X2
-
+  .connect(mongo_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((connection) => {
+    console.log(`Connected to Mongo Database name: "${connection.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
 
 const employeeRoute = require('./routes/employee.route.js');
 const app = express();
 app.use(bodyParser.json());
-app.use(
-    bodyParser.urlencoded({
-        extended: false,
-    }),
-)
-app.use(cors())
-app.use(express.static(path.join(__dirname, 'dist/mean-stack-crud-app')))
-app.use('/',express.static(path.join(__dirname, 'dist/mean-stack-crud-app')))
-app.use('/api',employeeRoute);
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
+app.use('/', express.static(path.join(__dirname, 'dist/mean-stack-crud-app')));
+app.use('/api', employeeRoute);
 
-const port = process.env.PORT || 4000
-const server = app.listen(port, ()=> {
-    console.log('Connected to port ' + port);
-})
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => {
+  console.log('Connected to port ' + port);
+});
 
-app.use((req,res,next)=> {
-    const err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-app.use(function (err,req,res,next) {
-    console.error(err.message)
-    if(!err.statusCode) err.statusCode = 500
-    res.status(err.statusCode).send(err.message)
-})
+app.use(function (err, req, res, next) {
+  console.error(err.message);
+  if (!err.statusCode) err.statusCode = 500;
+  res.status(err.statusCode).send(err.message);
+});
 
-
+// Exporting database name using process.env.DB_NAME
+module.exports = {
+  dbName: process.env.DB_NAME || 'default_db_name',
+};
